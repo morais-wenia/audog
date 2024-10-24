1aa16455bbdaa0c3e7ee89a67d8ae5adf67af413
morais-wenia <wenia.morais@fatec.sp.gov.br>
2024-10-24 10:49:54 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 // Wait for the document to be ready
 document.addEventListener('DOMContentLoaded', function () {
   // Create a map instance and set the initial view coordinates and zoom level
-  var map = L.map('map').setView([-23.52026413037824, -46.83670586136634], 13);
-
-
+  var map = L.map('map').setView([-23.516357107383552, -46.835143207955774], 7);
+ 
+ 
   // Add a tile layer to the map from OpenStreetMap
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map);
-
+ 
   var greenIcon = L.icon({
     iconUrl: 'https://e7.pngegg.com/pngimages/761/75/png-clipart-the-dog-will-laugh-animal-corgi.png',
  
@@ -16,8 +16,15 @@ document.addEventListener('DOMContentLoaded', function () {
     iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
-
+ 
   // Create a marker with popup and add it to the map
-  var marker = L.marker([-23.52026413037824, -46.83670586136634]), {icon: greenIcon}).addTo(map);
-  marker.bindPopup("fofo lindo fofo!").openPopup();
+  var marker = L.marker([-23.516357107383552, -46.835143207955774]).addTo(map);
+  marker.bindPopup("fofo lindo fofo perdido!").openPopup();
+ 
+ 
+  
+    // Create a marker with popup and add it to the map
+  var marker2 = L.marker([-23.52026413037824, -46.83670586136634], {icon: greenIcon}).addTo(map);
+  marker2.bindPopup("Amada turma de DMD, eu me perdi aqui").openPopup();
+ 
 });
